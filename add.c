0294d4c497fadf8c9fe1d56b90ed8d25be210c42
Taffy4467 <gandarit4467@gmail.com>
2022-12-14 06:58:11 -0800

    updating fiel

--- add.c
+++ add.c
@@ -13,6 +13,7 @@
 void _add(stack_t **stack, unsigned int line_cnt)
 {
 	int result;
+
 	if (!stack || !*stack || !((*stack)->next))
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", line_cnt);
